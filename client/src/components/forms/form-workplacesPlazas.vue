febd57e97efefecf155721422b14160b772ddc97
Betül YEL <yellbetul@gmail.com>
2024-08-08 11:42:22 +0300

    yelbetul

---
+++ client/src/components/forms/form-workplacesPlazas.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="overlay"></div>
+    <form action="#">
+        <div class="form-container">
+            <div class="form-header">
+                <div class="form-title">
+                    <h3>Renova Energie</h3>
+                    <h1>Arbeitsplätze und Plätze</h1>
+                </div>
+                <div class="icon">
+                    <span><i class="fa-solid fa-x"></i></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-element">
+                    <label for="name">Vorname Familienname</label>
+                    <input type="text" name="name" placeholder="Schreiben Sie Ihren Vor- und Nachnamen">
+                </div>
+                <div class="form-element">
+                    <label for="phone">Handynummer</label>
+                    <input type="number" name="phone">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-element">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email">
+                </div>
+                <div class="form-element">
+                    <label for="city">Stadt</label>
+                    <select name="city" id="city">
+                        <option value="null">--Wählen--</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-element">
+                    <label for="city">Bezirk</label>
+                    <select name="city" id="city">
+                        <option value="null">--Wählen--</option>
+                    </select>
+                </div>
+                <div class="form-element">
+                    <label for="city">Stationstyp</label>
+                    <select name="city" id="city">
+                        <option value="null">--Wählen--</option>
+                        <option value="AC">AC-Ladestation</option>
+                        <option value="null">DC-Hochgeschwindigkeitsladestation</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-textarea">
+                <label for="text">Erläuterung</label>
+                <textarea name="text" id="text"></textarea>
+            </div>
+            <div class="form-controls">
+                <button>Schicken</button>
+            </div>
+        </div>
+    </form>
+</template>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,16 +1,27 @@
 <template>
     <div>
-        <app-nav />
+        <app-form-individuell></app-form-individuell>
+        <app-form-WorkPlaza></app-form-WorkPlaza>
     </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
+import formIndividuell from '../components/forms/form-individual.vue'
+import formWorkplacesPlazas from '@/components/forms/form-workplacesPlazas.vue';
+import formCarPark from '@/components/forms/form-carPark.vue';
+import formGasStation from '@/components/forms/form-gasStation.vue';
+import formMuniciplaites from '@/components/forms/form-municiplaites.vue';
 export default {
     components:{
         'app-nav'   : Navbar,
-        'app-footer': Footer
+        'app-footer': Footer,
+        "app-form-individuell" : formIndividuell,
+        "app-form-WorkPlaza" : formWorkplacesPlazas,
+        "app-form-carPark" :formCarPark,
+        "app-form-gasStation" :formGasStation,
+        "app-form-municiplaites":formMuniciplaites
     }
 }
 </script>
